f2bf0499968c1b9461aec260cf8b2ef59bd4c754
Madhav A Das <116732718+madhav-a-das@users.noreply.github.com>
2023-05-03 23:44:15 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -19,3 +19,23 @@ root.addEventListener('mousemove', e => {
 function setPosition(element, e) {
   element.style.transform = `translate3d(${e.clientX}px, ${e.clientY}px, 0)`;
 }
+
+function update(value) {
+    var stretch = ['ultra-condensed','extra-condensed','condensed','semi-condensed','normal','semi-expanded','expanded','extra-expanded  ','ultra-expanded'];
+    document.getElementById("work").style.fontWeight=value*100;
+    document.getElementById("work").style.fontStretch=stretch[value-1];
+    document.getElementById("life").style.fontWeight=(1000-value*100);
+    document.getElementById("life").style.fontStretch=stretch[9-value];
+}
+  
+function updateColor() {
+  var redValue = document.getElementById("red-slider").value;
+  var greenValue = document.getElementById("green-slider").value;
+  var blueValue = document.getElementById("blue-slider").value;
+  var colorString = "rgb(" + redValue + "," + greenValue + "," + blueValue + ")";
+  // Todo: Update the text color
+  document.getElementById("color-sec").style.color=colorString;
+  document.getElementById("balance").style.color=colorString;
+  document.getElementById("rbg-slider").value=colorString;
+}
+  
